f1c2e2e932169729f7dfe7e99fc91367d3136490
mattmc3 <mattmc3@gmail.com>
2026-02-19 12:45:02 -0500

    Remove legacy ;and and ;or

--- conf.d/env.fish
+++ conf.d/env.fish
@@ -1,16 +1,16 @@
 # All variables are universals here. They can be overridden with globals in config.fish
 
 # Colorize man pages.
-set -q LESS_TERMCAP_mb; or set -Ux LESS_TERMCAP_mb (set_color -o blue)
-set -q LESS_TERMCAP_md; or set -Ux LESS_TERMCAP_md (set_color -o cyan)
-set -q LESS_TERMCAP_me; or set -Ux LESS_TERMCAP_me (set_color normal)
-set -q LESS_TERMCAP_so; or set -Ux LESS_TERMCAP_so (set_color -b white black)
-set -q LESS_TERMCAP_se; or set -Ux LESS_TERMCAP_se (set_color normal)
-set -q LESS_TERMCAP_us; or set -Ux LESS_TERMCAP_us (set_color -u magenta)
-set -q LESS_TERMCAP_ue; or set -Ux LESS_TERMCAP_ue (set_color normal)
+set -q LESS_TERMCAP_mb || set -Ux LESS_TERMCAP_mb (set_color -o blue)
+set -q LESS_TERMCAP_md || set -Ux LESS_TERMCAP_md (set_color -o cyan)
+set -q LESS_TERMCAP_me || set -Ux LESS_TERMCAP_me (set_color normal)
+set -q LESS_TERMCAP_so || set -Ux LESS_TERMCAP_so (set_color -b white black)
+set -q LESS_TERMCAP_se || set -Ux LESS_TERMCAP_se (set_color normal)
+set -q LESS_TERMCAP_us || set -Ux LESS_TERMCAP_us (set_color -u magenta)
+set -q LESS_TERMCAP_ue || set -Ux LESS_TERMCAP_ue (set_color normal)
 
 # Set editor variables.
-set -q PAGER; or set -Ux PAGER less
+set -q PAGER || set -Ux PAGER less
 
 if not set -q EDITOR
     switch $OSTYPE
@@ -24,5 +24,5 @@ end
 # Set browser on macOS.
 switch $OSTYPE
     case darwin
-        set -q BROWSER; or set -Ux BROWSER open
+        set -q BROWSER || set -Ux BROWSER open
 end
